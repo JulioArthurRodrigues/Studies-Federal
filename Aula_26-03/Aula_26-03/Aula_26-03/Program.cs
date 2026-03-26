e4720565666ff6b28590671cba0ab00603056bdf
Julio Arthur Rodrigues <julioarp2004@gmail.com>
2026-03-26 20:48:39 -0300

    Add files via upload

---
+++ Aula_26-03/Aula_26-03/Aula_26-03/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula_26_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Primeiro programa de exemplo usando o Visual Studio Code*/
+            Console.WriteLine("Hello, World!");
+            // O comando abaixo espera pressionar uma tecla para encerrar o programa
+            Console.ReadKey();  
+
+        }
+    }
+}
---
+++ Aula_26-03/Exemplo_01/Exemplo_01/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exemplo_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis inteiras
+            int num1, num2, soma, sub, mult, div;
+         
+            //Exibir mensagens e ler os números de entrada
+            Console.Write("Digite o Primeiro Número:");
+            num1 = int.Parse(Console.ReadLine());
+            
+            Console.Write("Digite o Segundo Número:");
+            num2 = int.Parse(Console.ReadLine());
+
+            //Limpeza de tela
+            Console.Clear();
+            Console.WriteLine("------RESULTADOS--------");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("");
+
+            //Faça os cálculos e atribua o valor para as variáveis
+            soma = num1 + num2;
+            sub = num1 - num2;
+            mult = num1 * num2;
+            div = num1 / num2;
+
+            //Exibir saídas
+            Console.WriteLine("Soma: {0}", soma);
+            Console.WriteLine("Subtração: {0}", sub);
+            Console.WriteLine("Multiplicação: {0}", mult);
+            Console.WriteLine("Divisão: {0}", div);
+
+            //Outro tipo de saída 
+            Console.WriteLine("A soma de {0} com {1} = {2}", num1, num2, soma);
+            
+            //Pausa para o encerramento do programa
+            Console.ReadKey();
+
+        }
+    }
+}
