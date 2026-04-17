750822f276f3cb536187b4509be80f6b42d4b025
Julio Arthur Rodrigues <julioarp2004@gmail.com>
2026-04-16 21:21:15 -0300

    Add files via upload

---
+++ Exemplo 2 - PRES 16_04/Exemplo2 PRES 16_04/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exemplo2_PRES_16_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao;
+            Console.WriteLine("Opções do Sistema");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("1. Cadastro");
+            Console.WriteLine("2. Alteração");
+            Console.WriteLine("3. Exclusão");
+            Console.WriteLine("4. Sair do Sistema");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Sua opção? : ");
+            opcao = Console.ReadLine();
+
+            opcao = opcao.ToUpper();
+
+            switch(opcao)
+            {
+                case "1":
+                case "C":
+                    Console.WriteLine("Você Escolheu Cadastro");
+                    break;
+                
+                case "2":
+                case "A":
+                    Console.WriteLine("Você Escolheu Alteração");
+                    break;
+                
+                case "3":
+                case "E":
+                    Console.WriteLine("Você Escolheu Exclusão");
+                    break;
+                
+                case "4":
+                case "F":
+                    Console.WriteLine("Você Escolheu Finalizar");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção Inválida");
+                    break;
+            }
+        }
+    }
+}
---
+++ Exercício 1 - Calculadora/Exercício 1 - Calculadora/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Exercício_1___Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, sum, sub, div, mult, pow;
+            Console.WriteLine("Digite dois números reais");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            string calculadora;
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("--------------");
+            Console.WriteLine("1. Soma");
+            Console.WriteLine("2. Subtração");
+            Console.WriteLine("3. Divisão");
+            Console.WriteLine("4. Multiplicação");
+            Console.WriteLine("5. Potenciação");
+            Console.WriteLine("---------------");
+            Console.WriteLine("Digite os dois números reais: ");
+            calculadora = Console.ReadLine();
+
+            switch(calculadora)
+            {
+                case "1":
+                case "Soma":
+                    Console.WriteLine("1. Some os dois números");
+                    Console.WriteLine("A soma entre os dois números reais é: {0}", sum = a + b);
+                    break;
+
+                case "2":
+                case "Subtração":
+                    Console.WriteLine("2. Subtraia os dois números");
+                    Console.WriteLine("A subtração entre os dois números reais é: {0}", sub = a - b);
+                    break;
+
+                case "3":
+                case "Divisão":
+                    Console.WriteLine("3. Divida os dois números");
+                    Console.WriteLine("A divisão entre os dois números reais é: {0}", div = a / b);
+                    break;
+
+                case "4":
+                case "Multiplicação":
+                    Console.WriteLine("4. Multiplique os dois números");
+                    Console.WriteLine("A multiplicação entre os dois números reais é: {0}", mult = a * b);
+                    break;
+
+                case "5":
+                case "Potenciação":
+                    Console.WriteLine("5. Faça a potenciação entre os dois números");
+                    Console.WriteLine("A potenciação entre os dois números reais é: {0}", Math.Pow(a,b));
+                    break;
+
+                 Console.WriteLine("\nPressione qualquer tecla para sair...");
+                 Console.ReadKey();
+            }
+        }
+    }
+}
